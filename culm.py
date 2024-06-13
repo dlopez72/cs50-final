7c5a298de00d1faf1b7a95f5465fbdfe33d3f147
dlopez72 <93018091+dlopez72@users.noreply.github.com>
2024-06-13 15:40:28 -0400

    MAJOR redo of classes and full block implementation

--- culm.py
+++ culm.py
@@ -1,18 +1,22 @@
 import pygame
-from random import randint
+from random import choice
 
-# TODO: try to seperate visual grid from placed info to make life easier.
 # TODO: implement full block shapes instead of individiual tiles
 
-# importantly for this code I made it that the shape of a tetromino directly
-# corresponds to its color so here's the guide:
-# 1 = YELLOW = O block
-# 2 = CYAN = I block
-# 3 = RED = Z block
-# 4 = GREEN = S block
-# 5 = ORANGE = L block
-# 6 = BLUE = J block
-# 7 = PURPLE = T block
+# the arrays on the right are the shapes of the blocks
+# eg the T is [
+# [0, 1, 0]
+# [1, 1, 1]
+# ] 
+tetrominoes = {
+    'O': {'color': "yellow", 'shape': [[1, 1], [1, 1]]},
+    'I': {'color': "cyan", 'shape': [[1, 1, 1, 1]]},
+    'Z': {'color': "red", 'shape': [[1, 1, 0], [0, 1, 1]]},
+    'S': {'color': "green", 'shape': [[0, 1, 1], [1, 1, 0]]},
+    'L': {'color': "orange", 'shape': [[1, 0, 0], [1, 1, 1]]},
+    'J': {'color': "blue", 'shape': [[0, 0, 1], [1, 1, 1]]},
+    'T': {'color': "purple", 'shape': [[0, 1, 0], [1, 1, 1]]}
+}
 
 # set up pygame stuff
 pygame.init()
@@ -21,83 +25,58 @@ screen = pygame.display.set_mode((250, 500))
 # run until user asks to quit
 running = True
 
-class Tile:
-    def __init__(self, location, past, color, attached):
-        self.location = location
-        self.past = past
-        self.color = color
-        self.attached = attached
-
-    def movement(self, keys):
-        # move block left if a is pressed, also makes the past location one tile right of the block's new location
-        if keys[pygame.K_a]:
-            if self.location[1] != 0 and game_grid[self.location[0]][self.location[1] - 1] == 0:
-                self.location[1] -= 1
-                self.past[1] = self.location[1] + 1
-            else:
-                self.past[1] = self.location[1]
-        # same thing but for right side
-        elif keys[pygame.K_d]:
-            if self.location[1] != 9 and game_grid[self.location[0]][self.location[1] + 1] == 0:
-                self.location[1] += 1
-                self.past[1] = self.location[1] - 1
-            else:
-                self.past[1] = self.location[1]
-        # if neither a or d are being touched it knows that it's last x location
-        # has to be the same
-        else:
-            self.past[1] = self.location[1]
-
-    def gravity(self):
-        if self.location[0] != 19 and game_grid[self.location[0] + 1][self.location[1]] == 0:
-            self.location[0] += 1
-            if self.location[0] != 0:
-                self.past[0] = self.location[0] - 1
-        else:
-            # keeps the current block location as filled while bringing the block back to the beginning
-            self.location = [0, 4]
-            self.past = [0, 4]
-            self.color = randint(1, 7)
-
-    def update_grid(self):
-        # reset the block's last location to empty
-        game_grid[self.past[0]][self.past[1]] = 0
-
-        # set grid to block location
-        game_grid[self.location[0]][self.location[1]] = self.color
-
-    
 class Tetromino:
-    def __init__(self, location, shape):
-        self.shape = shape
-        self.location = location
-        self.block1 = Tile(self.location, self.location, self.shape, [])
-        if self.shape == 1:
-            self.block2 = Tile([self.location[0], self.location[1]+1], [self.location[0], self.location[1]+1], self.shape, [])
-            self.block3 = Tile([self.location[0]+1, self.location[1]], [self.location[0]+1, self.location[1]], self.shape, [])
-            self.block4 = Tile([self.location[0]+1, self.location[1]+1], [self.location[0]+1, self.location[1]+1], self.shape, [])
-            self.block1.attached = [self.block2, self.block3, self.block4]
-            self.block2.attached = [self.block1, self.block3, self.block4]
-            self.block3.attached = [self.block1, self.block2, self.block4]
-            self.block4.attached = [self.block1, self.block2, self.block3]
+    def __init__(self):
+        self.shapecolor = choice(list(tetrominoes.keys()))
+        # uses the dict to assign a color and shape to the main key
+        self.color = tetrominoes[self.shapecolor]['color']
+        self.shape = tetrominoes[self.shapecolor]['shape']
+        self.position = [0, round(10 / 2) - round(len(self.shape[0]) / 2)]
 
+    # move block left if a is pressed and it's a valid position.
     def movement(self, keys):
-        self.block1.movement(keys)
-        self.block2.movement(keys)
-        self.block3.movement(keys)
-        self.block4.movement(keys)
-
+        if keys[pygame.K_a]:
+            self.position[1] -= 1
+            if not self.valid_position():
+                self.position[1] += 1
+        elif keys[pygame.K_d]:
+            self.position[1] += 1
+            if not self.valid_position():
+                self.position[1] -= 1
+        
     def gravity(self):
-        self.block1.gravity()
-        self.block2.gravity()
-        self.block3.gravity()
-        self.block4.gravity()
+        self.position[0] += 1
+        if not self.valid_position():
+            self.position[0] -= 1
+            self.lockin()
 
-    def update_grid(self):
-        self.block1.update_grid()
-        self.block2.update_grid()
-        self.block3.update_grid()
-        self.block4.update_grid()
+    # checks if the position you're trying to go to is valid
+    def valid_position(self):
+        for row_index, row in enumerate(self.shape):
+            for col_index, value in enumerate(row):
+                if value != 0:
+                    new_x = self.position[1] + col_index
+                    new_y = self.position[0] + row_index
+                    if new_x < 0 or new_x > 9 or new_y > 19 or (new_y >= 0 and game_grid[new_y][new_x] != 0):
+                        return False
+        return True
+    
+    # locks in current position into the main game grid.
+    def lockin(self):
+        for row_index, row in enumerate(self.shape):
+            for col_index, value in enumerate(row):
+                if value != 0:
+                    game_grid[self.position[0] + row_index][self.position[1] + col_index] = self.color
+        # i just realized you can call init again and OH MY GOD that makes life way easier.
+        self.__init__()
+
+    # drawing of tetronimoes is done seperately from placed grid, because doing them together was a nightmare
+    def draw(self):
+        for row_index, row in enumerate(self.shape):
+            for col_index, value in enumerate(row):
+                if value != 0:
+                    pygame.draw.rect(screen, self.color, ((self.position[1] + col_index) * 25, (self.position[0] + row_index) * 25, 25, 25))
+                
 
 # grid used for game
 #!!!! THE GRID IS IN Y, X !!!! [0][1] IS 1 TILE RIGHT OF THE TOP LEFT!!!
@@ -126,36 +105,22 @@ game_grid = [
 
 clock = pygame.time.Clock()
 time = 0
-tetro =  Tile([0, 4], [0, 4], 1, [])
+tetro =  Tetromino()
 interval = 30
 
 # main game loop
 while running:
+    screen.fill("black")
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    screen.fill("black")
     time += 1
 
     # basically multiplies the index by 25 to draw a 25 by 25 square at the appropriate place
     # relative to the grid. i found enumerate on google to have access to both the indices and values
     for row_index, row in enumerate(game_grid):
         for col_index, value in enumerate(row):
-            if value == 1:
-                pygame.draw.rect(screen, "yellow", (col_index * 25, row_index * 25, 25, 25))
-            elif value == 2:
-                pygame.draw.rect(screen, "cyan", (col_index * 25, row_index * 25, 25, 25))
-            elif value == 3:
-                pygame.draw.rect(screen, "red", (col_index * 25, row_index * 25, 25, 25))
-            elif value == 4:
-                pygame.draw.rect(screen, "green", (col_index * 25, row_index * 25, 25, 25))
-            elif value == 5:
-                pygame.draw.rect(screen, "orange", (col_index * 25, row_index * 25, 25, 25))
-            elif value == 6:
-                pygame.draw.rect(screen, "blue", (col_index * 25, row_index * 25, 25, 25))
-            elif value == 7:
-                pygame.draw.rect(screen, "purple", (col_index * 25, row_index * 25, 25, 25))
+            pygame.draw.rect(screen, value, (col_index * 25, row_index * 25, 25, 25))
 
     full_count = 0
 
@@ -183,12 +148,15 @@ while running:
         gravity_interval = 120
     else:
         gravity_interval = 30
+    movement_interval = 5
+    
+    tetro.draw()
 
-    # the interval dictactes how often the blocks move down a tile
+    # the interval dictactes how often the blocks move down a tile or are allowed to move
     if time % gravity_interval == 0:
-        tetro.movement(keys)
-        tetro.update_grid()
         tetro.gravity()
+    if time % movement_interval == 0:
+        tetro.movement(keys)
 
     # update the display
     pygame.display.flip()
