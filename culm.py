1350b800f6514cdf5619d701232c7be5cc14077d
Daniel Lopez <dlopezra@outlook.com>
2024-06-14 10:09:38 -0400

    Fix comment typo

--- culm.py
+++ culm.py
@@ -178,7 +178,7 @@ while running:
         for col_index, value in enumerate(row):
             pygame.draw.line(screen, "gray", [col_index * 25, 0], [col_index * 25, 500])
             pygame.draw.line(screen, "gray", [0, row_index * 25], [250, row_index * 25])
-    # two extra lies that dont get drawn in the for loop
+    # two extra lines that dont get drawn in the for loop
     pygame.draw.line(screen, "gray", [0, 499], [250, 499])
     pygame.draw.line(screen, "gray", [249, 0], [249, 500])
 
